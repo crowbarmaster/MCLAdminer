2f25fbeedd12d99ae6216cf348ec6597f756292d
tad tarrant <crowbarmasterhl2@gmail.com>
2020-10-29 18:10:41 -0400

    Cleaned up extra modifiers.

--- MCL_Adminer/Forms/Rooms/RoomForm.Designer.cs
+++ MCL_Adminer/Forms/Rooms/RoomForm.Designer.cs
@@ -14,72 +14,105 @@
 
 		private void InitializeComponent()
 		{
-			this.textBox1 = new global::System.Windows.Forms.TextBox();
-			this.label1 = new global::System.Windows.Forms.Label();
-			this.label2 = new global::System.Windows.Forms.Label();
-			this.textBox2 = new global::System.Windows.Forms.TextBox();
-			this.label6 = new global::System.Windows.Forms.Label();
-			this.button1 = new global::System.Windows.Forms.Button();
-			this.checkedListBox1 = new global::System.Windows.Forms.CheckedListBox();
-			base.SuspendLayout();
-			this.textBox1.Font = new global::System.Drawing.Font("Microsoft Sans Serif", 12f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 0);
-			this.textBox1.Location = new global::System.Drawing.Point(130, 42);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new global::System.Drawing.Size(50, 26);
-			this.textBox1.TabIndex = 0;
-			this.label1.AutoSize = true;
-			this.label1.Font = new global::System.Drawing.Font("Microsoft Sans Serif", 14f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 0);
-			this.label1.Location = new global::System.Drawing.Point(130, 10);
-			this.label1.Name = "label1";
-			this.label1.Size = new global::System.Drawing.Size(129, 24);
-			this.label1.TabIndex = 6;
-			this.label1.Text = "Add/edit room";
-			this.label2.AutoSize = true;
-			this.label2.Font = new global::System.Drawing.Font("Microsoft Sans Serif", 12f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 0);
-			this.label2.Location = new global::System.Drawing.Point(47, 45);
-			this.label2.Name = "label2";
-			this.label2.Size = new global::System.Drawing.Size(77, 20);
-			this.label2.TabIndex = 7;
-			this.label2.Text = "Room ID:";
-			this.textBox2.Font = new global::System.Drawing.Font("Microsoft Sans Serif", 12f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 0);
-			this.textBox2.Location = new global::System.Drawing.Point(13, 115);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new global::System.Drawing.Size(246, 26);
-			this.textBox2.TabIndex = 1;
-			this.label6.AutoSize = true;
-			this.label6.Font = new global::System.Drawing.Font("Microsoft Sans Serif", 12f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 0);
-			this.label6.Location = new global::System.Drawing.Point(47, 92);
-			this.label6.Name = "label6";
-			this.label6.Size = new global::System.Drawing.Size(162, 20);
-			this.label6.TabIndex = 8;
-			this.label6.Text = "Teacher/Room name:";
-			this.button1.Location = new global::System.Drawing.Point(130, 237);
-			this.button1.Name = "button1";
-			this.button1.Size = new global::System.Drawing.Size(129, 25);
-			this.button1.TabIndex = 11;
-			this.button1.Text = "Save room";
-			this.button1.UseVisualStyleBackColor = true;
-			this.checkedListBox1.Font = new global::System.Drawing.Font("Microsoft Sans Serif", 10f, global::System.Drawing.FontStyle.Regular, global::System.Drawing.GraphicsUnit.Point, 0);
-			this.checkedListBox1.FormattingEnabled = true;
-			this.checkedListBox1.Location = new global::System.Drawing.Point(285, 42);
-			this.checkedListBox1.Name = "checkedListBox1";
-			this.checkedListBox1.Size = new global::System.Drawing.Size(198, 220);
-			this.checkedListBox1.TabIndex = 12;
-			base.AutoScaleDimensions = new global::System.Drawing.SizeF(6f, 13f);
-			base.AutoScaleMode = global::System.Windows.Forms.AutoScaleMode.Font;
-			base.ClientSize = new global::System.Drawing.Size(489, 280);
-			base.Controls.Add(this.checkedListBox1);
-			base.Controls.Add(this.button1);
-			base.Controls.Add(this.label6);
-			base.Controls.Add(this.label2);
-			base.Controls.Add(this.label1);
-			base.Controls.Add(this.textBox2);
-			base.Controls.Add(this.textBox1);
-			base.Name = "RoomForm";
-			base.StartPosition = global::System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Form1";
-			base.ResumeLayout(false);
-			base.PerformLayout();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(195, 65);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(73, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Add/edit room";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Room ID:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(20, 177);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(367, 35);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(70, 142);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(247, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Teacher/Room name:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 365);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Save room";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(428, 65);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(295, 328);
+            this.checkedListBox1.TabIndex = 12;
+            // 
+            // RoomForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 431);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "RoomForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 		private global::System.ComponentModel.IContainer components = null;
 		private global::System.Windows.Forms.TextBox textBox1;
