b001310d7f883ab1f52687e683d924dbd3d15713
crowbarmaster <Freemanhl21.`>
2020-11-30 11:18:30 -0500

    Fixed delete function, added UpdateList() to manage listbox content changes.

--- MCL_Adminer/Forms/Rooms/ManageRooms.cs
+++ MCL_Adminer/Forms/Rooms/ManageRooms.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace MCL_Adminer
 {
     public partial class ManageRooms : Form
     {
+        int oldIndex;
         public ManageRooms()
         {
             InitializeComponent();
@@ -33,12 +35,14 @@ namespace MCL_Adminer
 
         private void button1_Click(object sender, EventArgs e)
         {
+            oldIndex = listBox1.SelectedIndex;
             FormProvider.RoomForm.Show();
             Hide();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            oldIndex = listBox1.SelectedIndex;
             if (listBox1.SelectedItem != null)
             {
                 foreach (Room room in Globals.RoomList)
@@ -54,15 +58,37 @@ namespace MCL_Adminer
         }
         private void button3_Click(object sender, EventArgs e)
         {
+            List<Room> tmp = Globals.RoomList;
+            oldIndex = listBox1.SelectedIndex;
             if (listBox1.SelectedItem != null)
             {
-                foreach (Room room in Globals.RoomList)
+                foreach (Room room in tmp)
                 {
                     if (listBox1.SelectedItem.ToString() == room.ID)
                     {
                         Globals.RoomList.Remove(room);
+                        new SQLHandler().Delete("rooms", $"rid={room.ID}");
+                        break;
                     }
                 }
+                UpdateList();
+            }
+        }
+        
+        public void UpdateList()
+        {
+            listBox1.Items.Clear();
+            foreach (Room rm in Globals.RoomList)
+            {
+                listBox1.Items.Add(rm.ID);
+            }
+            if (oldIndex > 0 && oldIndex < Globals.RoomList.Count)
+            {
+                listBox1.SetSelected(oldIndex, true);
+            }
+            else if (oldIndex == Globals.RoomList.Count && Globals.RoomList.Count > 0)
+            {
+                listBox1.SetSelected(Globals.RoomList.Count - 1, true);
             }
         }
     }
